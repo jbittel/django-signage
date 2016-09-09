b946bdbdb999adffc93b173e5dc0a230bf4e4eed
Jason Bittel <jason.bittel@gmail.com>
2016-09-09 15:29:16 -0700

    Create service to query slides endpoint

--- signage/static/app/app.js
+++ signage/static/app/app.js
@@ -1,90 +1,10 @@
-angular.module('signage', ['ngAnimate'])
+'use strict';
+
+angular.module('signage', [
+  'ngAnimate',
+  'signage.slide',
+])
 
 .config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-}])
-
-.controller('SignageController', ['$http', '$scope', '$timeout', function($http, $scope, $timeout) {
-  $scope.isCurrentSlide = function(index) {
-    return $scope.currentIndex === index;
-  };
-
-  function getSlideDuration() {
-    if ($scope.slides.length) {
-      return $scope.slides[$scope.currentIndex].duration * 1000;
-    } else {
-      return 0;
-    }
-  };
-
-  $scope.nextSlide = function() {
-    if ($scope.slides) {
-      $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-    }
-    $timeout($scope.nextSlide, getSlideDuration());
-  };
-
-  function updateSlides() {
-    $http.get('/display/1/slides/').then(function(response) {
-      if (!angular.equals($scope.slides, response.data)) {
-        $scope.slides = response.data;
-      }
-    });
-
-    $timeout(updateSlides, 10000);
-  };
-
-  $scope.currentIndex = 0;
-  $scope.slides = [];
-
-  updateSlides();
-  $scope.nextSlide();
-}])
-
-.directive('bgImage', ['$window', function($window) {
-  return function(scope, element, attrs) {
-    var resizeBG = function() {
-      var bgwidth = element.width();
-      var bgheight = element.height();
-
-      var winwidth = $window.innerWidth;
-      var winheight = $window.innerHeight;
-
-      var widthratio = winwidth / bgwidth;
-      var heightratio = winheight / bgheight;
-
-      var widthdiff = heightratio * bgwidth;
-      var heightdiff = widthratio * bgheight;
-
-      if (heightdiff > winheight) {
-        element.css({
-          width: winwidth + 'px',
-          height: winheight + 'px'
-        });
-      } else {
-        element.css({
-          width: widthdiff + 'px',
-          height: heightdiff + 'px'
-        });
-      }
-    };
-
-    var windowElement = angular.element($window);
-    windowElement.resize(resizeBG);
-
-    element.bind('load', function() {
-      resizeBG();
-    });
-  }
-}])
-
-.animation('.animate', function() {
-  return {
-    enter: function(element, done) {
-      TweenMax.fromTo(element, 1, {opacity: 0}, {opacity: 1, onComplete: done});
-    },
-    leave: function(element, done) {
-      TweenMax.to(element, 1, {opacity: 0, onComplete: done});
-    }
-  };
-});
+}]);
---
+++ signage/static/app/slide/slide.js
@@ -0,0 +1,96 @@
+angular.module('signage.slide', ['djng.urls'])
+
+.controller('SignageController', ['$scope', '$timeout', 'slideService', function($scope, $timeout, slideService) {
+  $scope.isCurrentSlide = function(index) {
+    return $scope.currentIndex === index;
+  };
+
+  function getSlideDuration() {
+    if ($scope.slides.length) {
+      return $scope.slides[$scope.currentIndex].duration * 1000;
+    } else {
+      return 0;
+    }
+  }
+
+  $scope.nextSlide = function() {
+    if ($scope.slides) {
+      $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+    }
+    $timeout($scope.nextSlide, getSlideDuration());
+  };
+
+  function updateSlides() {
+    slideService.getSlides()
+      .then(function(slides) {
+        if (!angular.equals($scope.slides, slides)) {
+          $scope.slides = slides;
+        }
+      });
+    $timeout(updateSlides, 10000);
+  }
+
+  $scope.currentIndex = 0;
+  $scope.slides = [];
+
+  updateSlides();
+  $scope.nextSlide();
+}])
+
+.service('slideService', ['$http', 'djangoUrl', function($http, djangoUrl) {
+  this.getSlides = function() {
+    // TODO Remove hard-coded display ID
+    return $http.get(djangoUrl.reverse('signage:display_slides', {'pk': '1'}))
+      .then(function(response) {
+        return response.data;
+      });
+  };
+}])
+
+.directive('bgImage', ['$window', function($window) {
+  return function(scope, element, attrs) {
+    var resizeBG = function() {
+      var bgwidth = element.width();
+      var bgheight = element.height();
+
+      var winwidth = $window.innerWidth;
+      var winheight = $window.innerHeight;
+
+      var widthratio = winwidth / bgwidth;
+      var heightratio = winheight / bgheight;
+
+      var widthdiff = heightratio * bgwidth;
+      var heightdiff = widthratio * bgheight;
+
+      if (heightdiff > winheight) {
+        element.css({
+          width: winwidth + 'px',
+          height: winheight + 'px'
+        });
+      } else {
+        element.css({
+          width: widthdiff + 'px',
+          height: heightdiff + 'px'
+        });
+      }
+    };
+
+    var windowElement = angular.element($window);
+    windowElement.resize(resizeBG);
+
+    element.bind('load', function() {
+      resizeBG();
+    });
+  };
+}])
+
+.animation('.animate', function() {
+  return {
+    enter: function(element, done) {
+      TweenMax.fromTo(element, 1, {opacity: 0}, {opacity: 1, onComplete: done});
+    },
+    leave: function(element, done) {
+      TweenMax.to(element, 1, {opacity: 0, onComplete: done});
+    }
+  };
+});
